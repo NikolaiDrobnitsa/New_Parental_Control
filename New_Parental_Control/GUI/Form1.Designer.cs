87d1eb29342014aac10d7c85db04a49febe5e825
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-07-22 22:58:23 +0300

    add some func

--- New_Parental_Control/GUI/Form1.Designer.cs
+++ New_Parental_Control/GUI/Form1.Designer.cs
@@ -28,79 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.All_App_listBox = new System.Windows.Forms.ListBox();
-            this.LockApplistBox = new System.Windows.Forms.ListBox();
-            this.UnLockAppListBox = new System.Windows.Forms.ListBox();
-            this.TempLockApplistBox = new System.Windows.Forms.ListBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.RefreshToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.LockToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.UnlockToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.TImeLockToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.ExapleTImetoolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // All_App_listBox
-            // 
-            this.All_App_listBox.FormattingEnabled = true;
-            this.All_App_listBox.Location = new System.Drawing.Point(12, 23);
-            this.All_App_listBox.Name = "All_App_listBox";
-            this.All_App_listBox.Size = new System.Drawing.Size(120, 95);
-            this.All_App_listBox.TabIndex = 0;
-            // 
-            // LockApplistBox
-            // 
-            this.LockApplistBox.FormattingEnabled = true;
-            this.LockApplistBox.Location = new System.Drawing.Point(176, 23);
-            this.LockApplistBox.Name = "LockApplistBox";
-            this.LockApplistBox.Size = new System.Drawing.Size(120, 95);
-            this.LockApplistBox.TabIndex = 1;
-            // 
-            // UnLockAppListBox
-            // 
-            this.UnLockAppListBox.FormattingEnabled = true;
-            this.UnLockAppListBox.Location = new System.Drawing.Point(339, 23);
-            this.UnLockAppListBox.Name = "UnLockAppListBox";
-            this.UnLockAppListBox.Size = new System.Drawing.Size(120, 95);
-            this.UnLockAppListBox.TabIndex = 2;
-            // 
-            // TempLockApplistBox
-            // 
-            this.TempLockApplistBox.FormattingEnabled = true;
-            this.TempLockApplistBox.Location = new System.Drawing.Point(500, 23);
-            this.TempLockApplistBox.Name = "TempLockApplistBox";
-            this.TempLockApplistBox.Size = new System.Drawing.Size(120, 95);
-            this.TempLockApplistBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Location = new System.Drawing.Point(36, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RefreshToolStripButton,
+            this.LockToolStripButton,
+            this.UnlockToolStripButton,
+            this.TImeLockToolStripButton,
+            this.ExapleTImetoolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // RefreshToolStripButton
+            // 
+            this.RefreshToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("RefreshToolStripButton.Image")));
+            this.RefreshToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.RefreshToolStripButton.Name = "RefreshToolStripButton";
+            this.RefreshToolStripButton.Size = new System.Drawing.Size(81, 22);
+            this.RefreshToolStripButton.Text = "Обновить";
+            // 
+            // LockToolStripButton
+            // 
+            this.LockToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("LockToolStripButton.Image")));
+            this.LockToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LockToolStripButton.Name = "LockToolStripButton";
+            this.LockToolStripButton.Size = new System.Drawing.Size(111, 22);
+            this.LockToolStripButton.Text = "Заблокировать";
+            // 
+            // UnlockToolStripButton
+            // 
+            this.UnlockToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("UnlockToolStripButton.Image")));
+            this.UnlockToolStripButton.ImageTransparentColor = System.Drawing.Color.Lime;
+            this.UnlockToolStripButton.Name = "UnlockToolStripButton";
+            this.UnlockToolStripButton.Size = new System.Drawing.Size(116, 22);
+            this.UnlockToolStripButton.Text = "Разблокировать";
+            // 
+            // TImeLockToolStripButton
+            // 
+            this.TImeLockToolStripButton.Image = global::New_Parental_Control.Properties.Resources._108181;
+            this.TImeLockToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.TImeLockToolStripButton.Name = "TImeLockToolStripButton";
+            this.TImeLockToolStripButton.Size = new System.Drawing.Size(168, 22);
+            this.TImeLockToolStripButton.Text = "Временно заблокировать";
+            // 
+            // ExapleTImetoolStripComboBox1
+            // 
+            this.ExapleTImetoolStripComboBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.ExapleTImetoolStripComboBox1.Name = "ExapleTImetoolStripComboBox1";
+            this.ExapleTImetoolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 49);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip2.TabIndex = 6;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(48, 22);
+            this.toolStripLabel1.Text = "Фильтр";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 74);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 376);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название приложения";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Состояние приложения";
+            this.columnHeader2.Width = 200;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.TempLockApplistBox);
-            this.Controls.Add(this.UnLockAppListBox);
-            this.Controls.Add(this.LockApplistBox);
-            this.Controls.Add(this.All_App_listBox);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox All_App_listBox;
-        private System.Windows.Forms.ListBox LockApplistBox;
-        private System.Windows.Forms.ListBox UnLockAppListBox;
-        private System.Windows.Forms.ListBox TempLockApplistBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton RefreshToolStripButton;
+        private System.Windows.Forms.ToolStripButton LockToolStripButton;
+        private System.Windows.Forms.ToolStripButton UnlockToolStripButton;
+        private System.Windows.Forms.ToolStripButton TImeLockToolStripButton;
+        private System.Windows.Forms.ToolStripComboBox ExapleTImetoolStripComboBox1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- New_Parental_Control/GUI/Form1.cs
+++ New_Parental_Control/GUI/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,13 +13,20 @@ namespace New_Parental_Control
 {
     public partial class Form1 : Form
     {
+        private List<Process> processes = null;
         public Form1()
         {
             InitializeComponent();
         }
+        private void GetProcesses()
+        {
+            processes.Clear();
+            processes = Process.GetProcesses().ToList<Process>();
 
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+
         }
     }
 }
